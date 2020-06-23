90bd8ea13ca75f08580eb430b47987ad5f6f186f
unanyanrazmik <unanyan.razmik@mail.ru>
2020-06-24 01:00:46 +0300

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import DescriptionContainer from "./components/Description/DescriptionContainer";
-import {Route} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import RepositoriesContainer from "./components/Repositories/RepositoriesContainer";
 
 
 function App() {
   return (
     <div className="App">
+        <Switch>
         <Route path="/repositories" component={RepositoriesContainer}/>
         <Route path="/description/:id?" component={DescriptionContainer}/>
+        <Route path="/" component={() => <Redirect to = '/repositories'/>}/>
+        </Switch>
     </div>
   );
 }
